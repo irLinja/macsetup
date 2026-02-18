a057005b5acda1cedc0ba399d38266c2b2985592
Arash Haghighat <arash@a12t.co>
2026-02-18 23:27:04 +0100

    fix(04-01): use non-deprecated settings.user.name/email in git.nix
    - Replace obsolete userName/userEmail with settings.user.name/email
    - Eliminates Home Manager deprecation warnings during evaluation

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -2,9 +2,6 @@
   programs.git = {
     enable = true;
 
-    userName = "Arash Haghighat";
-    userEmail = "arash@a12t.co";
-
     signing = {
       key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+8Td4M6GvHcuOr9J/syhqqymwroVg6e7qRyKtlwacYVMeWUQnxMZf/XeJBjZsJ/MT82n32JYg/gNU9AG97maLzRleqTd/3B2gpRkCokliCQvcUI18U/WmmfYsA+pgvLfvbGgUzod41AoQbJlYBTCaF5JBq8ljSUZot0dg4/pv+NwjleiwO/+b5k0IcFkUSgNTpmFMH8St1VAjrEW8aQQA+NZ/j3pxomIqIlFJLwPPiNp0RsLc5vb41QTAAnaZlaVw9Ue9ZxjR+BzSPGp7c/ZuV9V1AFfaovRc/evSfIR9BFSL2PdcjF8u/7LKiKdkIxVfBq28bcgfNkZ1XpEYA5chKoPhr22QbUKkwAimu+Q2iA4AmZrwfYcE6PvtbL+E5xf7YsKqI9d+CATgPEmzW4ZtxzElT/XJyYs7XxQDYB47Py2w+GdA6mQw4uBlkyDnncmW5xf7xFAgTMzLljqRUm/6Vz/TTDrpvDQUaQ8MA9yHBWCd5J+4aPG2pyExFIY9m/vyg9VEGTibVzpL+fLrRHX+SUAtgnrFwA1LjIWnu14UllQelPqePqaijWfb6kpxpxa+Ez3eBcG6utprHmFJPJ5uY0vsV1T4FXR4QegbLcqMWmrOGX8EW2v+23VhdAfFYBxAGG2omp21W4IAKleJ82OC06sfBnbRlComiyTBKrr9tw==";
       signByDefault = true;
@@ -35,6 +32,10 @@
     ];
 
     settings = {
+      user = {
+        name = "Arash Haghighat";
+        email = "arash@a12t.co";
+      };
       init.defaultBranch = "main";
       core = {
         editor = "vim";
