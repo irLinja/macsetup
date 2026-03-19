d472bf1ce628cc6b945c558abbf63f2feac1321d
Arash Haghighat <arash@a12t.co>
2026-03-19 15:38:09 +0100

    feat(hosts): add FYMX72C301 work machine with username override
    Overrides username to arash.haghighat for this host while all other
    machines use the default arash from user.nix.

---
+++ hosts/FYMX72C301.nix
@@ -0,0 +1,46 @@
+# FYMX72C301 -- work machine (Just Eat Takeaway)
+{ inputs, userConfig, pkgs, lib, ... }:
+let
+  hostUserConfig = userConfig // { username = "arash.haghighat"; };
+in {
+  imports = [
+    ../profiles/work
+    ../modules/darwin
+    ../modules/optional/1password.nix
+  ];
+
+  # System identity (overridden for this host)
+  system.primaryUser = hostUserConfig.username;
+
+  users.users.${hostUserConfig.username} = {
+    name = hostUserConfig.username;
+    home = "/Users/${hostUserConfig.username}";
+  };
+
+  # Home Manager integration
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.extraSpecialArgs = { inherit inputs; userConfig = hostUserConfig; };
+  home-manager.backupFileExtension = "backup";
+  home-manager.users.${hostUserConfig.username} = import ../modules/home;
+
+  # Declarative Homebrew (nix-homebrew)
+  nix-homebrew = {
+    enable = true;
+    enableRosetta = false;
+    user = hostUserConfig.username;
+    mutableTaps = false;
+    taps = {
+      "homebrew/homebrew-core" = inputs.homebrew-core;
+      "homebrew/homebrew-cask" = inputs.homebrew-cask;
+      "peonping/homebrew-tap" = inputs.homebrew-peonping;
+      "tfversion/homebrew-tap" = inputs.homebrew-tfversion;
+    };
+  };
+
+  # -- Host-specific overrides -----------------------------------------
+  # Add packages on top of your profile's defaults:
+  # home-manager.users.${hostUserConfig.username}.home.packages = with pkgs; [
+  #   mongosh
+  # ];
+}
